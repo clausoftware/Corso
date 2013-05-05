7c89fed4bc17facb7158d18582ad7981426d45a2
adinaG <adina_costache_88@yahoo.com>
2013-05-05 19:13:56 +0300

    ...

--- src/orders/Product.java
+++ src/orders/Product.java
@@ -5,6 +5,7 @@ public class Product {
 
 	public Product(int x) {
 		x = 5;
+		int y = x;
 	}
 	public Product() {
 		System.out.println("Fu-tu-ti pizda matii. Exact asta voiam sa fac. Si Product nu are ce cauta aici");
